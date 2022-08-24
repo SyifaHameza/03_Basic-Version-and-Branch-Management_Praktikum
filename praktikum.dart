171ec3d740f48b221648061312594b3cfa82493b
Syifa Hameza Hamid <syifa.hameza@gmail.com>
2022-08-25 03:57:27 +0700

    Melakukan merge feature dengan development

--- praktikum.dart
+++ praktikum.dart
@@ -1,10 +1,7 @@
 void main(List<String> args) {
   print('Hi! Ini adalah branch master');
   print('Halo, kalau ini branch development');
-<<<<<<< HEAD
   print('Kalau ini adalah branch featureA');
-=======
   print('Kalau ini adalah branch featureB');
   print('Ini adalah stash');
->>>>>>> featureB
 }
